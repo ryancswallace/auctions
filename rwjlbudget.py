57b0e98524eaa72a8b5a4b915b84a2717c655a0a
ryanwallace96 <ryanwallace@college.harvard.edu>
2017-10-26 07:25:11 -0400

    adding budget agent

--- rwjlbudget.py
+++ rwjlbudget.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
 import sys
+import math
 
 from gsp import GSP
 from util import argmax_index
 
-class BBAgent:
+class rwjlbudget_cos:
     """Balanced bidding agent"""
     def __init__(self, id, value, budget):
         self.id = id
         self.value = value
         self.budget = budget
+        self.N_ROUNDS = 48
+        self.attractiveness_history = []
+        self.top_bid = [10e10]
 
     def initial_bid(self, reserve):
         return self.value / 2
@@ -78,12 +82,42 @@ class BBAgent:
         # (p_x is the price/click in slot x)
         # If s*_j is the top slot, bid the value v_j
 
-        prev_round = history.round(t-1)
-        (slot, min_bid, max_bid) = self.target_slot(t, history, reserve)
+        # remaining_budget = self.budget - history.agents_spent[self.id]
+        # rounds_remaining = self.N_ROUNDS - t
+        # pos_1 = 30 * math.cos(math.pi * t / 24) + 50
 
-        # TODO: Fill this in.
-        bid = 0  # change this
-        
+        # top_slot_clicks = remaining_budget * pos_1 / (50 * (rounds_remaining + 1))
+        # bid = top_slot_clicks
+        # if self.id == 1:
+        #     print "ROUND", t
+        #     print 'factor', pos_1 / (50 * (rounds_remaining + 1))
+        #     print 'remaining budget', remaining_budget
+        #     print
+
+        ########
+
+        memory_length = 3
+
+        if t > 1:
+            self.top_bid.append(max([b for (_, b) in history.round(t-1).bids]))
+
+            bids = []
+            for lag in range(1, min(memory_length, t) + 1):
+                bids += [b for (_, b) in history.round(t-lag).bids]
+            
+            price_level = sum(bids) / float(len(bids))
+            normed_value = 30 * math.cos(math.pi * t / 24) + 50
+            attractiveness = normed_value / price_level
+            attractiveness_delta = (attractiveness - self.attractiveness_history[-1]) / float(attractiveness)
+        else:
+            attractiveness = 1
+            attractiveness_delta = 0
+
+        self.attractiveness_history.append(attractiveness)
+
+        base_bid = 600 / 48.0
+        bid = base_bid * (1 + attractiveness_delta)
+
         return bid
 
     def __repr__(self):
